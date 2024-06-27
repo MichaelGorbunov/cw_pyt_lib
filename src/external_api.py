a994c385594894331e7c38b095dcbf7eed988ab8
Michael Gorbunov <mg0rby@yandex.ru>
2024-06-27 16:24:25 +0300

    test external_api

--- src/external_api.py
+++ src/external_api.py
@@ -1,4 +1,4 @@
-import json
+# import json
 import logging
 import os
 import random
@@ -7,7 +7,7 @@ from typing import Any, Dict, Union
 import requests
 from dotenv import load_dotenv
 
-from config import LOGS_DIR, ROOT_DIR, url, url_stocks
+from config import LOGS_DIR, url, url_stocks
 
 load_dotenv()
 
@@ -22,66 +22,51 @@ logger.addHandler(logger_file_handler)
 logger.setLevel(logging.DEBUG)
 
 
-def get_currency_fake(currency: str) -> float:
-    result = round(random.uniform(85.7, 90.3), 2)
-    logger.info(result)
-    return result
 
-
-def get_stock_fake(stock: str) -> float:
-    result = round(random.uniform(185.7, 190.3), 2)
-    logger.info(result)
-    return result
-
-
-# print(get_currency_fake("US"))
-# print(get_stock_fake("APPL"))
 
 
 def getting_data_currencies(currencies: dict) -> list[Dict] | Any:
     """Функция, которая получает данные о курсе валют из указанного API для заданной валюты"""
     result = []
     api = url
-    try:
-        logger.info("Перебираем валюты из заданного списка словарей валют")
-        for currency in currencies["user_currencies"]:
-            response = requests.get(api, params={"get": "rates", "pairs": f"{currency}RUB", "key": API_KEY})
-            logger.debug(response.text)
 
-            response_json = response.json()
-            logger.info("Полученные данные преобразовываем в заданный словарь")
-            result.append({"currency": currency, "rate": list(response_json["data"].values())[0]})
-        logger.debug(result)
-        return result
-    except requests.exceptions.RequestException:
-        logger.error("Запрос на сервис API не успешный")
-        return []
+    logger.info("Перебираем валюты из заданного списка словарей валют")
+    for currency in currencies["user_currencies"]:
+        response = requests.get(api, params={"get": "rates", "pairs": f"{currency}RUB", "key": API_KEY})
+        if response.status_code != 200:
+            logger.error("Ошибка запроса")
+            return []
+        logger.debug(response.text)
 
-
-with open(os.path.join(ROOT_DIR, "user_settings.json"), "r") as f:
-    data_json = json.load(f)
-    logger.info(data_json)
+        response_json = response.json()
+        logger.info("Полученные данные преобразовываем в заданный словарь")
+        # result.append({"currency": currency, "rate": list(response_json["data"].values())[0]})
+        result.append({"currency": currency, "rate": list(response_json["data"].values())[0]})
+    logger.debug(result)
+    return result
 
 
 def getting_data_stock_prices(stocks: dict) -> Union[list[dict] | dict]:
     """Функция, которая получает данные о ценах акции из указанного API для заданной акции"""
     result = []
     api = url_stocks
-    try:
-        for stock in stocks["user_stocks"]:
-            logger.info("Делаем запрос на сервис API для получения цен акций")
-            response = requests.get(api, params={"symbol": stock, "token": API_KEY_STOCKS})
 
-            logger.info("Получаем JSON данные")
-            response_json = {"stock": stock, "data": response.json()}
-            logger.info(response_json)
+    for stock in stocks["user_stocks"]:
+        logger.info("Делаем запрос на сервис API для получения цен акций")
+        response = requests.get(api, params={"symbol": stock, "token": API_KEY_STOCKS})
+        if response.status_code != 200:
+            logger.error("Ошибка запроса")
+            return []
 
-            logger.info("Полученные данные преобразовываем в заданный словарь")
-            result.append({"stock": response_json["stock"], "price": response_json["data"]["c"]})
+        logger.info("Получаем JSON данные")
+        response_json = {"stock": stock, "data": response.json()}
+        logger.info(response_json)
 
-        logger.info("Выводим результат")
-        logger.debug(result)
-        return result
-    except requests.exceptions.RequestException:
-        logger.error("Запрос на сервис API не успешный")
-        return []
+        logger.info("Полученные данные преобразовываем в заданный словарь")
+        # result.append({"stock": response_json["stock"], "price": response_json["data"]["c"]})
+        result.append({"stock": response_json["stock"], "price": response_json.get("data").get("c")})
+
+    logger.info("Выводим результат по акциям")
+    logger.debug(result)
+    return result
+
---
+++ tests/test_external_api.py
@@ -0,0 +1,39 @@
+import json
+from unittest.mock import patch
+import os
+from src.external_api import getting_data_currencies,getting_data_stock_prices
+from config import ROOT_DIR
+with open(os.path.join(ROOT_DIR, "tests\\user_settings_test.json"), "r") as f:
+    data_json = json.load(f)
+
+
+
+@patch("src.external_api.requests.get")
+def test_getting_data_stock_prices_invalid(mocked_get):
+    """ошибка запроса к API"""
+    mocked_get.return_value.status_code = 400
+    result = getting_data_currencies(data_json)
+    assert result == []
+
+@patch("src.external_api.requests.get")
+def test_getting_data_stock_prices_invalid(mocked_get):
+    """ошибка запроса к API"""
+    mocked_get.return_value.status_code = 400
+    result = getting_data_stock_prices(data_json)
+    assert result == []
+
+@patch("src.external_api.requests.get")
+def test_currency_conversion(mocked_get):
+    mocked_get.return_value.status_code = 200
+    mocked_get.return_value.json.return_value = {'data': {'USDRUB': '64.1824'}}
+    result = getting_data_currencies(data_json)
+    assert result ==  [{'currency': 'USD', 'rate': '64.1824'}]
+
+@patch("src.external_api.requests.get")
+def test_getting_data_stock_prices(mocked_get):
+    mocked_get.return_value.status_code = 200
+    mocked_get.return_value.json.return_value = {'stock': 'AAPL', 'data': {'c': 213.25, 'd': 4.18, 'dp': 1.9993, 'h': 214.86, 'l': 210.64, 'o': 211.5, 'pc': 209.07, 't': 1719360000}}
+    result = getting_data_stock_prices(data_json)
+    assert result ==  [{'stock': 'AAPL', 'price': None} ]
+
+
