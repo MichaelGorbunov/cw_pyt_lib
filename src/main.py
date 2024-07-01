0ea10642c800dfbf1ebb84eecacb508be8ee2cf0
Michael Gorbunov <mg0rby@yandex.ru>
2024-07-01 08:05:53 +0300

    fic

--- src/main.py
+++ src/main.py
@@ -6,33 +6,24 @@ from src.views import get_views_data
 
 def main() -> None:
     """запуск скриптов для получения данных в интерактивном режиме"""
-    while True:
-        json_view = input("вывести данные для веб страницы(да/нет) : ").lower()
-        if json_view == "да":
-            date = input("Введите дату в формате YYYY-MM-DD HH:MM:SS: ")
-            print(get_views_data(date))
-            break
-        else:
-            break
-    while True:
-        json_view = input("вывести данные переводов физическим лицам(да/нет) : ").lower()
-        if json_view == "да":
-            data_frame = get_dataframe()
-            print(get_transfer_people(data_frame))
-            break
-        else:
-            break
-    while True:
-        json_view = input("вывести траты по категории (да/нет): ").lower()
-        if json_view == "да":
-            date = input("Введите дату в формате YYYY-MM-DD :")
-            category = input("Введите категорию : ")
-            data_frame = get_dataframe()
-            category_data = spending_by_category(data_frame, category, date)
-            print(category_data)
-            break
-        else:
-            break
+
+    json_view = input("вывести данные для веб страницы(да/нет) : ").lower()
+    if json_view == "да":
+        date = input("Введите дату в формате YYYY-MM-DD HH:MM:SS: ")
+        print(get_views_data(date))
+
+    json_view = input("вывести данные переводов физическим лицам(да/нет) : ").lower()
+    if json_view == "да":
+        data_frame = get_dataframe()
+        print(get_transfer_people(data_frame))
+
+    json_view = input("вывести траты по категории (да/нет): ").lower()
+    if json_view == "да":
+        date = input("Введите дату в формате YYYY-MM-DD :")
+        category = input("Введите категорию : ")
+        data_frame = get_dataframe()
+        category_data = spending_by_category(data_frame, category, date)
+        print(category_data)
 
 
 if __name__ == "__main__":
